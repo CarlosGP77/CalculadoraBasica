8b295023d1167db850acb17249ad1d76e8df39cb
Carlos González <carlos7enero@gmail.com>
2025-12-01 13:26:39 +0100

    Primer commit: Suma y Resta

---
+++ src/main/java/calculator.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class calculator {
+    public static void main(String[] args){
+        double numero1, numero2, resultado = 0;
+        int operacion;
+        char op = 'o';
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce el primer número: ");
+        numero1 = scanner.nextDouble();
+
+        System.out.print("Introduce el segundo número: ");
+        numero2 = scanner.nextDouble();
+
+        System.out.print("Introduce la operación (+ = 1  , - = 2): ");
+        do {
+            operacion = scanner.nextInt();
+
+            switch (operacion) {
+                case 1:
+                    resultado = numero1 + numero2;
+                    op ='+';
+                    break;
+                case 2:
+                    resultado = numero1 - numero2;
+                    op ='-';
+                    break;
+                default:
+                    System.out.println("Operación no válida. Vuelva a intentarlo.");
+                    operacion = 0;
+                    break;
+            }
+        }while (operacion == 0);
+        System.out.println(numero1 + " " + op + " " + numero2 + " = " + resultado);
+
+        scanner.close();
+    }
+}
