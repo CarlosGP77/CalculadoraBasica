87d87fa34dffa25765c118cc34f2da4edbfea47e
Carlos González <carlos7enero@gmail.com>
2025-12-14 19:05:40 +0100

    Añadido mcm y mcd

--- src/main/java/calculator.java
+++ src/main/java/calculator.java
@@ -49,7 +49,7 @@ public class calculator {
                     operacion = 0;
                     break;
             }
-        } while (operacion == 0);
+        } while (operacion == 0); //Repetir bucle
 
         // Solo mostrar resultado si es una operación aritmética
         if (operacion >= 1 && operacion <= 4) {
